6f6c1d697e7970abe8ac3befaa317b2604e74fb2
saniok2525 <saniok2525@gmail.com>
2019-11-14 18:28:19 +0000

    Comment

--- js/index.js
+++ js/index.js
@@ -1,16 +1,39 @@
-const display = document.querySelector('.calc .display');
-document.querySelectorAll('.calc .digits button, .calc .opers button')
+const rezult = document.querySelector('.calculator .rezult'),
+memoryNewNumber = false;
+
+document.querySelectorAll('.calculator .digits button, .calculator .opers button')
     .forEach( button => button.addEventListener('click', digitOperPressed));
 
 function digitOperPressed(event) {
     const btnText = event.target.innerText;
-    display.value += btnText;
+    rezult.value += btnText;
 }
 
-document.querySelector('.calc .eq')
+document.querySelector('.calculator .equal')
     .addEventListener('click', eqPressed);
 
 function eqPressed() {
-    display.value = eval(display.value);
+    rezult.value = eval(rezult.value);
 }
 
+document.querySelector('.calculator .deleteAll')
+    .addEventListener('click', deleteAllPressed);
+
+function deleteAllPressed () {
+    rezult.value = rezult.value = "";
+}
+
+
+document.querySelector('.calculator .deleteLast')
+    .addEventListener('click', dltPressed);
+function dltPressed() {
+    const numbers = rezult.value;
+    rezult.value = numbers.slice(0, -1);
+} 
+
+document.querySelectorAll('.calculator .opers button').innerText
+    .addEventListener('click', sameOper);
+    function sameOper() {
+        const sameSymbol = document.querySelectorAll('.calculator .opers button').innerText;
+        if(rezult.value === sameSymbol){fail;}
+    }
